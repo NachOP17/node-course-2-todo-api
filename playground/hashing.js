07845c6f95effbeef2f2a2e040a4c16e52531999
Ignacio <ignaciotfw@gmail.com>
2017-10-15 20:03:50 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,19 @@
 const {SHA256} = require('crypto-js');
 const {MD5} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded', decoded);
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded', decoded);
 // var message = 'I am user number 1';
 // var hash = SHA256(message).toString();
 // var md5 = MD5(message).toString();
@@ -37,3 +40,17 @@ console.log('Decoded', decoded);
 // } else {
 //   console.log('Data was changed. Do not trust!');
 // }
+
+var password = 'abcq123';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$1/QYkgXTdq2nhzzzKq14UOJK0hGMGqXCBswCuUjDx5HyyXU9Y6d7q';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
